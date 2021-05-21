641798dae95cd0947d70bdbffaf6c9a2f3388634
ashnilp <ashnilp@gmail.com>
2021-05-21 17:06:17 +1200

    home

---
+++ pou/src/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+   return (
+       <div className="home-container">
+         <div className="header">
+            Ashnil
+         </div>
+         <div className="main">
+
+         </div>
+         <div className="footer">
+
+         </div>
+       </div>
+   )
+}
+export default Home;
